965580dd434ba0d02a63e4905a42ee06f6a3662a
Javier Antiles <javier_susy_nike@yahoo.com>
2018-01-18 02:08:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
-  elseif number % 15 == 0
+  elsif number % 15 == 0
     "Buzz"
-  elseif number % 5 == 0
+  elsif number % 5 == 0
     "FizzBuzz"
   else
     nil
